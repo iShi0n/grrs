cb3a605d9750686692a0bc7cded301bd3094a970
Guilherme <guilherme0xff@proton.me>
2022-08-26 16:25:58 -0300

    feat: adiciona filtro por pattern, e flag `color`

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
-fn main() {
-    println!("Hello, world!");
+use ansi_term::Color::Red;
+use anyhow::{Context, Result};
+use clap::Parser;
+
+#[derive(Parser)]
+struct Cli {
+    pattern: String,
+    #[clap(parse(from_os_str))]
+    path: std::path::PathBuf,
+    #[clap(long)]
+    color: bool,
+}
+
+#[derive(Debug)]
+struct CustomError(String);
+
+fn main() -> Result<()> {
+    let args = Cli::parse();
+    let _enabled = ansi_term::enable_ansi_support();
+
+    let content = std::fs::read_to_string(&args.path)
+        .with_context(|| format!("could not read `{}`", &args.path.display()))?;
+
+    for line in content.lines() {
+        let index_found = line.find(&args.pattern);
+
+        if index_found != None {
+            if args.color {
+                println!(
+                    "{}",
+                    line.replace(&args.pattern, &Red.paint(&args.pattern).to_string())
+                );
+            } else {
+                println!("{}", line);
+            }
+        }
+    }
+
+    Ok(())
 }

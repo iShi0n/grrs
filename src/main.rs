5dad6961f0fcee5e04c5c2299e868061311c3af1
Guilherme <guilherme0xff@proton.me>
2022-08-26 18:00:23 -0300

    feat: adiciona `--ignore-case`

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,14 @@ use clap::Parser;
 #[derive(Parser)]
 #[clap(author, version)]
 struct Cli {
-    pattern: String,
-    #[clap(parse(from_os_str))]
-    path: std::path::PathBuf,
     /// Highlight matching string
     #[clap(long)]
     color: bool,
+    #[clap(short, long)]
+    ignore_case: bool,
+    pattern: String,
+    #[clap(parse(from_os_str))]
+    path: std::path::PathBuf,
 }
 
 #[derive(Debug)]
@@ -24,17 +26,22 @@ fn main() -> Result<()> {
         .with_context(|| format!("could not read `{}`", &args.path.display()))?;
 
     for line in content.lines() {
-        let index_found = line.find(&args.pattern);
+        let index_found;
+
+        if args.ignore_case {
+            index_found = line.to_lowercase().find(&args.pattern.to_lowercase());
+        } else {
+            index_found = line.find(&args.pattern);
+        }
 
         if index_found == None {
             continue;
         }
 
         if args.color {
-            println!(
-                "{}",
-                line.replace(&args.pattern, &Red.paint(&args.pattern).to_string())
-            );
+            let found = &line[index_found.unwrap()..args.pattern.len()];
+
+            println!("{}", line.replace(found, &Red.paint(found).to_string()));
         } else {
             println!("{}", line);
         }
